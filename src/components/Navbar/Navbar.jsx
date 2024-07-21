75c04cf192aead06c77c9c6c26c099d8450625c2
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2024-07-21 16:59:33 +0530

    Added navbar , quizcard

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom"
+export const Navbar = ( ) =>{
+
+    return (
+        <header className="heading d-flex grow-shrink-basis align-center">
+        <div className="heading-title-icon d-flex grow-shrink-basis align-center">
+            <h1 className="heading-title">
+                <Link className="link" to="/">Quizo</Link>
+            </h1>
+        </div>
+        <nav className="navigation">
+            <ul className="list-non-bullet">
+                <li className="list-item-inline">
+                    <Link to="#" className="link cursor">Home</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
+
